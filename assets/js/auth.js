75fb12f0aa76a8b624c2e8e34db1b03257f12c49
Dmytro Hubin <dimkanishe2099@gmail.com>
2023-08-01 19:47:33 +0300

    login submit styles

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -26,39 +26,41 @@ refs.authCloseBtn.addEventListener("click", () => {
 
 refs.authSubmit.addEventListener("click", (e) => {
   e.preventDefault();
-  refs.emailLoginValidation.classList.add('visually-hidden')
-  refs.passwordLoginValidation.classList.add('visually-hidden')
+  refs.emailLoginValidation.classList.add("visually-hidden");
+  refs.passwordLoginValidation.classList.add("visually-hidden");
 
-  let isFormValidated = true
+  let isFormValidated = true;
 
   if (!refs.emailLoginField.value) {
-    refs.emailLoginValidation.classList.remove('visually-hidden');
-    isFormValidated = false
+    refs.emailLoginValidation.classList.remove("visually-hidden");
+    isFormValidated = false;
   }
 
   if (!refs.passwordLoginField.value) {
-    refs.passwordLoginValidation.classList.remove('visually-hidden');
-    isFormValidated = false
+    refs.passwordLoginValidation.classList.remove("visually-hidden");
+    isFormValidated = false;
   }
 
   if (isFormValidated == true) {
+    document.querySelector(".auth-modal__form-submit").setAttribute("disabled", "true");
     const payload = new FormData();
-  payload.append("log", refs.emailLoginField.value);
-  payload.append("pwd", refs.passwordLoginField.value);
-  axios.post("wp-login.php", payload).then((response) => {
-    if (response.data.length > 1) {
-      document
-        .querySelector(".invalid-input-message.req-error")
-        .classList.remove("visually-hidden");
-    } else {
-      document
-        .querySelector(".invalid-input-message.req-error")
-        .classList.remove("visually-hidden");
-      document.querySelector(".invalid-input-message.req-error").textContent =
-        "Ви успішно авторизувалися!";
-      document.querySelector(".invalid-input-message.req-error").style.color = "green";
-      location.reload()
-    }
-  })
+    payload.append("log", refs.emailLoginField.value);
+    payload.append("pwd", refs.passwordLoginField.value);
+    axios.post("wp-login.php", payload).then((response) => {
+      document.querySelector(".auth-modal__form-submit").removeAttribute("disabled");
+      if (response.data.length > 1) {
+        document
+          .querySelector(".invalid-input-message.req-error")
+          .classList.remove("visually-hidden");
+      } else {
+        document
+          .querySelector(".invalid-input-message.req-error")
+          .classList.remove("visually-hidden");
+        document.querySelector(".invalid-input-message.req-error").textContent =
+          "Ви успішно авторизувалися!";
+        document.querySelector(".invalid-input-message.req-error").style.color = "green";
+        location.reload();
+      }
+    });
   }
 });
